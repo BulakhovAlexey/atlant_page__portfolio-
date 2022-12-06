e1cd0ba75f9e5bce37b6a29c97a9f1ce34fe87d0
BulakhovAlexey <bulakhovl@mail.ru>
2022-12-06 18:24:02 +0400

    завершена верстка блоков страницы, добавлены: валидация форм ввода, всплывающие окна

--- js/main.js
+++ js/main.js
@@ -1,91 +1,4 @@
-let volumeButtons = document.querySelectorAll(".volume_item_btn");
-volumeButtons.forEach((btn, index) => {
-  btn.onclick = function () {
-    document.querySelector(".volume__inner .active").classList.remove("active");
-    this.classList.add("active");
-    document.querySelector(
-      ".volume__image"
-    ).style.background = `url(../img/${index}.png) no-repeat center`;
-    document.querySelector(".volume__image").style.backgroundSize = "contain";
-  };
-});
-// slider
-const swiper = new Swiper(".swiper", {
-  speed: 1000,
-  initialSlide: 2,
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
-    clickable: true,
-  },
-  effect: "cards",
-  cardsEffect: {
-    perSlideOffset: 7,
-    slideShadows: false,
-    rotate: false,
-    perSlideRotate: 0,
-  },
-});
-let questionBtns = document.querySelectorAll(".questions_block__link");
-questionBtns.forEach((btn, indexBtn) => {
-  btn.onclick = function () {
-    document
-      .querySelector(".questions_block__link_active")
-      .classList.remove("questions_block__link_active");
-    this.classList.add("questions_block__link_active");
-    document.querySelector(".answer_active").classList.remove("answer_active");
-    document
-      .getElementById(`answer_${indexBtn}`)
-      .classList.add("answer_active");
-  };
-});
-
-// валидация номера телефона
-function validateUserPhone(phone) {
-  let reg = /^[a-z0-9_-]{3,16}$/;
-  return reg.test(String(phone));
-}
-let inputs = document.querySelectorAll(".js-input");
-let form = document.querySelector(".form_input");
-let userName = document.getElementById("userName");
-let userPhone = document.getElementById("userPhone");
-
-// обработка сабмита
-form.onsubmit = function () {
-  let userNameVal = document.getElementById("userName").value;
-  let userPhoneVal = document.getElementById("userPhone").value;
-  let emptyInputs = Array.from(inputs).filter((input) => input.value === "");
-  inputs.forEach((input) => {
-    if (input.value == "") {
-      input.classList.add("error");
-    } else {
-      input.classList.remove("error");
-    }
-  });
-  if (emptyInputs.length !== 0) {
-    return false;
-  }
-  if (!validateUserName(userNameVal)) {
-    userName.classList.add("error");
-    return false;
-  } else {
-    userName.classList.remove("error");
-  }
-  if (!validateUserPhone(userPhoneVal)) {
-    userPhone.classList.add("error");
-    return false;
-  } else {
-    userPhone.classList.remove("error");
-  }
-  return true;
-};
-
-// скрол к ссылкам
+// плавнфый скрол к ссылкам
 (function () {
   const smoothScroll = function (targetEl, duration) {
     let target = document.querySelector(targetEl);
@@ -113,9 +26,123 @@ form.onsubmit = function () {
     links.forEach((each) => {
       each.addEventListener("click", function () {
         const currentTarget = this.getAttribute("href");
-        smoothScroll(currentTarget, 2000);
+        smoothScroll(currentTarget, 1000);
       });
     });
   };
   scrollTo();
 })();
+
+// блок Volume
+let volumeButtons = document.querySelectorAll(".volume_item_btn");
+volumeButtons.forEach((btn, index) => {
+  btn.onclick = function () {
+    document.querySelector(".volume__inner .active").classList.remove("active");
+    this.classList.add("active");
+    document.querySelector(
+      ".volume__image"
+    ).style.background = `url(../img/${index}.png) no-repeat center`;
+    document.querySelector(".volume__image").style.backgroundSize = "contain";
+  };
+});
+
+// slider
+const swiper = new Swiper(".swiper", {
+  speed: 1000,
+  initialSlide: 2,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  effect: "cards",
+  cardsEffect: {
+    perSlideOffset: 7,
+    slideShadows: false,
+    rotate: false,
+    perSlideRotate: 0,
+  },
+});
+
+//block callback
+let questionBtns = document.querySelectorAll(".questions_block__link");
+questionBtns.forEach((btn, indexBtn) => {
+  btn.onclick = function () {
+    document
+      .querySelector(".questions_block__link_active")
+      .classList.remove("questions_block__link_active");
+    this.classList.add("questions_block__link_active");
+    document.querySelector(".answer_active").classList.remove("answer_active");
+    document
+      .getElementById(`answer_${indexBtn}`)
+      .classList.add("answer_active");
+  };
+});
+
+// Функция валидации номера телефона
+function validateUserPhone(phone) {
+  let reg = /^[a-z0-9_-]{3,16}$/;
+  return reg.test(String(phone));
+}
+
+// функция проверки на пустые поля и корректный номер телефона
+function validateEmptyFields(inputClassName, userPhoneIdName) {
+  let formInputs = document.querySelectorAll(inputClassName);
+  let userPhoneVal = document.getElementById(userPhoneIdName).value;
+  let emptyInputs = Array.from(formInputs).filter(
+    (input) => input.value === ""
+  );
+  formInputs.forEach((input) => {
+    if (input.value == "") {
+      input.classList.add("error");
+    } else {
+      input.classList.remove("error");
+    }
+  });
+  if (emptyInputs.length !== 0) {
+    return false;
+  }
+  if (!validateUserPhone(userPhoneVal)) {
+    userPhone.classList.add("error");
+    return false;
+  } else {
+    userPhone.classList.remove("error");
+  }
+  return true;
+}
+
+// обработка сабмита form1
+let form1 = document.querySelector(".form_input");
+let thankYou = document.getElementById("popup_thankYou");
+form1.onsubmit = function (e) {
+  e.preventDefault();
+  if (!validateEmptyFields(".js-input", "userPhone")) {
+    return false;
+  } else {
+    thankYou.classList.add("active_thankYou");
+    return true;
+  }
+};
+
+// обработка сабмита form2
+let form2 = document.querySelector(".popup_form_input");
+form2.onsubmit = function (e) {
+  e.preventDefault();
+  if (!validateEmptyFields(".popup-input", "CallBackUserPhone")) {
+    return false;
+  } else {
+    thankYou.classList.add("active_thankYou");
+    return true;
+  }
+};
+
+// закрытие окна спасибо
+let backBtn = document.querySelector(".backBtn");
+backBtn.addEventListener("click", () => {
+  thankYou.classList.remove("active_thankYou");
+});
